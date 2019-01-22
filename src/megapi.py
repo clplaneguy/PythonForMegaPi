22b3d2153fe9848779d339fcd7c3e6773c4b34a7
clplaneguy <clplaneguy@gmail.com>
2019-01-22 16:36:34 -0600

    Clarify version-for software

--- src/megapi.py
+++ src/megapi.py
@@ -4,9 +4,11 @@ import sys
 
 if sys.version > '3':
     PY3 = True
-    print("\r\nVersion:" + sys.version)
+    print("\r\nPython Version: " + sys.version);
+    print("megapi.py Version: 0.2.0");
     from megapi_python3 import *
 else:
     PY3 = False
-    print("\r\nVersion:" + sys.version)
+    print("\r\nPython Version: " + sys.version);
+    print("megapi.py Version: 0.2.0");
     from megapi_python2 import *
